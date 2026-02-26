e63e46ccdbdcb9de23f8101d96b216920c0629cd
jasonw040409 <tongyang.w@wustl.edu>
2026-02-26 14:08:10 -0600

    commit

--- src/test/SpellCheckerTest.java
+++ src/test/SpellCheckerTest.java
@@ -7,8 +7,90 @@ import org.junit.jupiter.api.Test;
 class SpellCheckerTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testWordCount() {
+		SpellChecker checker = new SpellChecker();
+		int words = checker.getNumberOfWords();
+		assertEquals(0, words);
 	}
+
+	@Test
+	void testAddWord() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("Helloworld");
+		int words = checker.getNumberOfWords();
+		assertEquals(1, words);
+	}
+
+	@Test
+	void testAlreadyAddedWord() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("Helloworld");
+		checker.addWord("Helloworld");
+		int words = checker.getNumberOfWords();
+		assertEquals(1, words);
+	}
+
+	@Test
+	void testProperlySpelledWord() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("Helloworld");
+		boolean isSpelledCorrectly = checker.isSpelledCorrectly("Helloworld");
+		assertTrue(isSpelledCorrectly);
+	}
+
+	@Test
+	void testMisspelledWord() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("Hlloworld");
+		boolean isSpelledCorrectly = checker.isSpelledCorrectly("Hlloworld");
+		assertFalse(isSpelledCorrectly);
+	}
+
+	@Test
+	void testIgnoreCase() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("caT");
+		boolean isSpelledCorrectly = checker.isSpelledCorrectly("cat");
+		assertTrue(isSpelledCorrectly);
+		checker.addWord("Cat");
+		isSpelledCorrectly = checker.isSpelledCorrectly("cat");
+		assertTrue(isSpelledCorrectly);
+	}
+
+	@Test
+	void testMisspelledSuggestion() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("bank");
+		String suggestion = checker.getSuggestion("bamk");
+		assertEquals("bank", suggestion);
+	}
+
+	@Test
+	void testCorrectSuggestion() {
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("bank");
+		String suggestion = checker.getSuggestion("bank");
+		assertEquals("bank", suggestion);
+	}
+
+	@Test
+	void testRemoveWord(){
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("helloworld");
+		int words = checker.getNumberOfWords();
+		assertEquals(1, words);
+		checker.removeWord("helloworld");
+		words = checker.getNumberOfWords();
+		assertEquals(0, words);
+	}
+
+	@Test
+	void testCapitalizeAll(){
+		SpellChecker checker = new SpellChecker();
+		checker.addWord("helloworld");
+		String capitalized = checker.capitalizeAll();
+		assertEquals("HELLOWORLD", capitalized);
+	}
+
 
 }
